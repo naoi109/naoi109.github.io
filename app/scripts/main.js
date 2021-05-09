4994022fca2d290347c0b2f1c05175b00dee1d6e
Naomi Isogai <naoi109@gmail.com>
2021-05-09 20:26:59 +0900

    記述修正

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -154,10 +154,10 @@ $(window).on('load', function() {
 $(function(){
   $('a[href^="#"]').click(function(){
     let speed = 700;
-    let href= $(this).attr("href");
-    let target = $(href == "#" || href == "" ? 'html' : href);
+    let href= $(this).attr('href');
+    let target = $(href == '#' || href == '' ? 'html' : href);
     let position = target.offset().top;
-    $("html, body").animate({scrollTop:position}, speed, "swing");
+    $('html, body').animate({scrollTop:position}, speed, 'swing');
     return false;
   });
 });
